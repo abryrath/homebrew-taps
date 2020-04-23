1049ad44c2f71787915f6af038a16d270d02e9b7
Abry Rath <abry.rath@union.co>
2020-04-23 10:22:08 -0400

    Removing version from lrb

--- lrm.rb
+++ lrm.rb
@@ -3,7 +3,6 @@ class Lrm < Formula
   homepage "https://github.com/abryrath/lrm"
   url "https://github.com/abryrath/lrm/releases/download/0.1/lrm-0.1.tar.gz"
   sha256 "53a8803ca17d11e9c0f08edf9627627e191eacc032cb9633f7f5ec825c2ae975"
-  version "0.1"
 
   def install
     bin.install "release/lrm"
